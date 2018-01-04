34756a02c4ec3b709622ae3f6f6266117a0285bf
Raman Choudhary <ramanc@syncroutesoftware.com>
2018-01-04 14:34:18 +0530

    added mp photos

---
+++ images/Members _ Lok Sabha_files/Global_Search_Validation.js
@@ -0,0 +1,49 @@
+﻿
+         var specialKeys = new Array();
+         specialKeys.push(8); //Backspace
+         specialKeys.push(9); //Tab
+         specialKeys.push(13); //Enter
+         specialKeys.push(32); //space
+         specialKeys.push(46); //Delete
+         specialKeys.push(36); //Home
+         specialKeys.push(35); //End
+         specialKeys.push(37); //Left
+         specialKeys.push(39); //Right
+         function IsAlphaNumeric(e) {
+
+
+             var keyCode = e.keyCode == 0 ? e.charCode : e.keyCode;
+
+             var ret = ((keyCode >= 48 && keyCode <= 57) || (keyCode == 32) || (keyCode == 13) || (keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122) || (specialKeys.indexOf(e.keyCode) != -1 && e.charCode != e.keyCode));
+             document.getElementById("error").style.display = ret ? "none" : "inline";
+             return ret;
+         }
+
+
+         function IsAlphaNumericPast() {
+
+             var spclChars = "!@#$%^&*()+=-[]\\\';,./{}|\":<>?";
+
+
+
+             var content = document.getElementById("txtSearchGlobal").value;
+             if (content.length < 3) {
+                 alert("Minimum 3 characters are needed");
+                 //document.getElementById("txtSearchGlobal").value = "";
+                 return false;
+             }
+             else{
+                 for (var i = 0; i < content.length; i++) {
+                     if (spclChars.indexOf(content.charAt(i)) != -1) {
+                         alert("Special characters are not allowed.");
+                         document.getElementById("txtSearchGlobal").value = "";
+                         return false;
+                     }
+                     
+                 }
+
+             }
+
+         }
+
+ 
--- script/get_full_mp_info.js
+++ script/get_full_mp_info.js
@@ -1,3 +1,22 @@
 (function(){
   console.log("loaded");
+
+  fetch('./data/2014/mp_list.0.1.json')
+  .then(mplist => mplist.json())
+  .then(mplist => {
+
+    openProfile(mplist[0].profileURL)
+    .then(res => {
+      const html = $(res).find("#pnlDiv1");
+      $('body').append(html)
+      console.log(html);
+    });
+  })
 })(window,$)
+
+
+function openProfile(url){
+  return fetch(url)
+  .then(res => res.text())
+  .catch(e => console.error(e))
+}
